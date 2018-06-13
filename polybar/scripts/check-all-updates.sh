d69b2cca54d3c20b002ee012068793cca673a710
Erik Dubois <erik.dubois@gmail.com>
2018-06-13 17:15:17 +0200

    improving the script to get the updates

--- polybar/scripts/check-all-updates.sh
+++ polybar/scripts/check-all-updates.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+updates_arch=0
+updates_aur=0
+
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
@@ -12,7 +15,7 @@ fi
 updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
-    echo "# $updates"
+    echo " $updates"
 else
     echo "0"
 fi
