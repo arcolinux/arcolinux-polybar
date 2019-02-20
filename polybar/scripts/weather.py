43f6b0e01ae65da0e24cb5191fbb6690c97c8bc2
kmitv <wojtek.kmita@hotmail.com>
2019-02-20 15:29:54 +0100

    added dynamic weather icon functionality

--- polybar/scripts/weather.py
+++ polybar/scripts/weather.py
@@ -13,14 +13,15 @@
 # LANG included thanks to krive001 on discord
 
 
-import requests
+import requests, time, calendar
 
-CITY = "2803138"
+CITY = "3094802"
 API_KEY = "756edce7e9d4c385ef9499a53492678c"
 UNITS = "Metric"
 UNIT_KEY = "C"
 #UNIT_KEY = "F"
-LANG = "en"
+# LANG = "en"
+LANG = "pl"
 #LANG = "nl"
 #LANG = "hu"
 
@@ -30,8 +31,49 @@ try:
     if REQ.status_code == 200:
         CURRENT = REQ.json()["weather"][0]["description"].capitalize()
         TEMP = int(float(REQ.json()["main"]["temp"]))
-        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+        ID = int(REQ.json()["weather"][0]["id"])
+        SUNSET = REQ.json()["sys"]["sunset"]
+        SUNRISE = REQ.json()["sys"]["sunrise"]
+        CURRENT_TIME = calendar.timegm(time.gmtime())
+        
+        #GROUP 2XX: THUNDERSTORM       
+        if ID >= 200 and ID < 300:
+            ID_FA = ""
+        #GROUP 3XX: DRIZZLE 
+        elif ID >= 300 and ID < 400:
+            ID_FA = ""
+        #GROUP 5XX: RAIN
+        elif ID >= 500 and ID < 600:
+            ID_FA = ""
+        #GROUP 6XX: SNOW
+        elif ID >= 600 and ID < 700:
+            ID_FA = ""
+        #GROUP 7XX: ATMOSPHERE
+        elif ID >= 700 and ID < 800:
+            ID_FA = ""
+        #GROUP 80X: CLOUDS
+        elif ID > 800 and ID < 900:
+            if ID == 801:
+                #DAY
+                if CURRENT_TIME >= SUNRISE and CURRENT_TIME <= SUNSET:
+                    ID_FA = ""
+                #NIGHT
+                else:
+                    ID_FA = ""
+            else:
+                ID_FA = ""
+        #GROUP 800: CLEAR
+        elif ID == 800:
+            #DAY
+            if CURRENT_TIME >= SUNRISE and CURRENT_TIME <= SUNSET:
+                ID_FA = ""
+            #NIGHT
+            else:
+                ID_FA = ""
+        else:
+            ID_FA = " "
+        print("{} {}, {} °{}".format(ID_FA, CURRENT , TEMP, UNIT_KEY))
     else:
         print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
 except (ValueError, IOError):
-    print("Error: Unable print the data")
+    print("Error: Unable to print the data")
