191fd99a35c4133c0a914ec50d523604e534f0e5
John Blevins <samurailostinjapan@gmail.com>
2020-10-15 12:36:26 +0900

    updated aur script to use yay

--- etc/skel/.config/polybar/scripts/check-all-updates.sh
+++ etc/skel/.config/polybar/scripts/check-all-updates.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #source https://github.com/x70b1/polybar-scripts
-
+#source https://github.com/polybar/polybar-scripts
 
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+#if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
+# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
